4395ff9a17e9c06f2ceef34a3f148f0b0a4af9a4
Arpit Tyagi <arpittyagi0748@gmail.com>
2026-03-24 19:02:28 +0530

    Android Support

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.shakelab.app',
+  appName: 'ShakeLab',
+  webDir: 'dist'
+};
+
+export default config;
